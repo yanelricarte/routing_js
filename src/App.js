39b55eb116c273b2118bee202b2c5ad80ba653e3
Yanel <yanelricarte@gmail.com>
2023-08-21 13:49:55 -0300

    subiendo repo

--- src/App.js
+++ src/App.js
@@ -9,16 +9,16 @@ function App() {
   return (
     <>
       <h1>React App</h1>
-       <BrowserRouter basename="/routing_js">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/producto/:id" element={<MostrarProducto />} />
-          <Route path="/registro" element={<Registro />} />
-          <Route path="/login" element={<Login />} />
-        </Routes>
-      </BrowserRouter> 
-    </>
-  );
-}
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/producto/:id" element={<MostrarProducto />} />
+            <Route path="/registro" element={<Registro />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </BrowserRouter> 
+      </>
+    );
+  }
 
 export default App;

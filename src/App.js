84375d09542bd901d4c050d06de53bf7ce4fa4c9
Yanel <yanelricarte@gmail.com>
2023-08-21 12:57:17 -0300

    subiendo repo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from "./componentes/home/home";
 import MostrarProducto from "./componentes/MostrarProducto";
 import Registro from "./componentes/Registro";
@@ -8,16 +8,15 @@ import Login from "./componentes/Login";
 function App() {
   return (
     <>
-    <h1> react App</h1>
-      <Router>
+      <h1>React App</h1>
+      <BrowserRouter basename="/routing_js">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/producto/:id" element={<MostrarProducto />} />
           <Route path="/registro" element={<Registro />} />
           <Route path="/login" element={<Login />} />
-
         </Routes>
-      </Router>
+      </BrowserRouter>
     </>
   );
 }
